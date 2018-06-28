634739fa57b54e23056775e61db96918049dc375
Christian Oder <myself5@carbonrom.org>
2018-06-27 21:14:43 -0400

    hanoip: avb: Build disabled VBMeta image
    Change-Id: I381feef8f6fefc8449ca50d85d704b67bcc8a77e
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,3 +174,10 @@ BOARD_RAMDISK_USE_LZMA := true
 # Dimensions
 TW_Y_OFFSET := 115
 TW_H_OFFSET := -115
+# Treble
+BOARD_VNDK_VERSION := current
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
