7661c144d6fabe91cc3c508022c44c9ac41cabec
vvr3ddy <vvr-rockstar@bmsit.in>
2022-06-12 18:44:24 +0530

    hanoip: Import configs from CAF tree - Ref: Atoll - Tag: LA.UM.9.1.r1-11700-SMxxx0.0
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,5 +7,132 @@
 DEVICE_PATH := device/motorola/hanoip
 BOARD_VENDOR := motorola
 
+# A/B
+ENABLE_VIRTUAL_AB := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+
+# AVB 2.0
+BOARD_AVB_ENABLE := true
+
+BOARD_AVB_VBMETA_SYSTEM := system
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
+
+BOARD_AVB_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_SYSTEM_ROLLBACK_INDEX := 0
+BOARD_AVB_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := false
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := atoll
+TARGET_NO_BOOTLOADER := true
+
+# Build Hacks
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_PREBUILT_ELF_FILES := true
+
+# Display
+TARGET_USES_ION := true
+TARGET_USES_NEW_ION_API :=true
+
+# DRM plugins
+TARGET_ENABLE_MEDIADRM_64 := true
+
+# DTBO image
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+# EFI
+TARGET_USES_UEFI := true
+
+# FM
+BOARD_HAVE_QCOM_FM := false
+
+# Metadata
+BOARD_USES_METADATA_PARTITION := true
+
+# OpenGL
+USE_OPENGL_RENDERER := true
+
+# Kernel
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
+BOARD_KERNEL_CMDLINE += \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    console=ttyMSM0,115200n8 \
+    earlycon=msm_geni_serial,0x4a90000 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    service_locator.enable=1 \
+    swiotlb=2048 \
+    video=vfb:640x400,bpp=32,memsize=3072000
+
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+TARGET_USES_UNCOMPRESSED_KERNEL := false
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x06000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 4294967296
+BOARD_METADATAIMAGE_PARTITION_SIZE := 16777216
+BOARD_DTBOIMG_PARTITION_SIZE := 0x0800000
+
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Platform
+TARGET_BOARD_PLATFORM := atoll
+QCOM_BOARD_PLATFORMS := atoll
+TARGET_USES_QSSI := true
+
+# Properties
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+
+# QG user space
+PMIC_QG_SUPPORT := true
+
+# QSPM compilation
+TARGET_USES_QSPM := true
+
+# Sensor multi HAL
+USE_SENSOR_MULTI_HAL := true
+
+# Treble flag
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+BOARD_VNDK_VERSION := current
+
 # Inherit from the proprietary version
 include vendor/motorola/hanoip/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -6,3 +6,104 @@
 
 # Inherit from vendor if exists
 $(call inherit-product-if-exists, vendor/motorola/hanoip/hanoip-vendor.mk)
+
+
+# Include GSI
+$(call inherit-product, build/make/target/product/gsi_keys.mk)
+
+# Enable Virtual A/B
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Audio
+PRODUCT_PACKAGES += libvolumelistener
+
+SYSTEMEXT_SEPARATE_PARTITION_ENABLE = true
+
+# Enable Dynamic partition
+BOARD_DYNAMIC_PARTITION_ENABLE := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# API level
+BOARD_SHIPPING_API_LEVEL := 30
+BOARD_API_LEVEL := 30
+
+# Fastboot
+PRODUCT_PACKAGES += fastbootd
+PRODUCT_PACKAGES += android.hardware.fastboot@1.0-impl-mock
+
+# F2FS
+PRODUCT_PACKAGES += \
+    f2fs_io \
+    check_f2fs \
+    sg_write_buffer
+
+# Userdata checkpoint
+AB_OTA_POSTINSTALL_CONFIG += \
+RUN_POSTINSTALL_vendor=true \
+POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+FILESYSTEM_TYPE_vendor=ext4 \
+POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc
+
+# Enable incremental FS feature
+PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
+
+# Manufacturer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=QTI
+
+# Hal Hardware
+TARGET_USES_AOSP := false
+TARGET_USES_AOSP_FOR_AUDIO := false
+TARGET_USES_QCOM_BSP := false
+
+# RRO configuration
+TARGET_USES_RRO := true
+
+# Kernel configurations
+TARGET_KERNEL_VERSION := 4.14
+KERNEL_LLVM_SUPPORT := true
+KERNEL_SD_LLVM_SUPPORT := true
+
+# A/B related packages
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
+    update_engine \
+    update_engine_client \
+    update_verifier
+
+PRODUCT_HOST_PACKAGES += \
+    brillo_update_payload
+
+# Boot control HAL
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+PRODUCT_PACKAGES += \
+    update_engine_sideload
+
+# QSPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qspm.enable=true
+
+# Telephony
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+PRODUCT_BOOT_JARS += tcmiface
+
+# Vendor property to enable advanced network scanning
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.enableadvancedscan=true
+
+# Property to disable ZSL mode
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_zsl_mode=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.crypto.volume.filenames_mode = "aes-256-cts" \
+ro.crypto.allow_encrypt_override = true
