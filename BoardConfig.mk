c1824a129ba6323883862acd1b4621d5fc2ed9ad
vvr3ddy <vvr-rockstar@bmsit.in>
2022-06-12 18:18:25 +0530

    hanoip: Initial device makefiles - Lineage Based
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

---
+++ BoardConfig.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/motorola/hanoip
+BOARD_VENDOR := motorola
+
+# Inherit from the proprietary version
+include vendor/motorola/hanoip/BoardConfigVendor.mk
