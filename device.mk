442278e13b5472a6b3e46ab8d6600f315dfbc6b7
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2022-06-20 19:25:05 +0530

    hanoip: FIX: Import config.fs from CAF
    Complete the incomplete commit : b9862e8e01e3cbe306356d02aa399fc14a5ffaaf
    
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- device.mk
+++ device.mk
@@ -174,6 +174,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@3.5.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Configs File System
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
