1f1ccf8661fc01fd9903512a2223bdd3dba70b3b
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2023-12-16 11:56:04 +0000

    hanoip: Define Kernel flags - Boot with prebuilts for now
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,16 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7
+BOARD_KERNEL_PAGESIZE := 4096
+TARGET_KERNEL_CONFIG := vendor/sm6150_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/sm6150
+
+# Kernel - Prebuilt
+include device/motorola/sm6225-kernel/prebuilt_kernel.mk
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := sm6150
